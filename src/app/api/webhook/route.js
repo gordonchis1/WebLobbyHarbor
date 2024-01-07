5add1c2d5e6c5dcd31af0417564ece027975224a
eduardo valdes <eduardovaldse@yahoo.com>
2024-01-06 18:12:36 -0600

    fix webhook for clerk

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,6 +1,12 @@
-/* eslint-disable camelcase */
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { dbConnect } from '../../../db/db'
+import User from '../../../db/models/User'
+
+export async function GET(req) {
+  return NextResponse.json({ congratulatio: 'lfkadkj' })
+}
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -14,12 +20,12 @@ export async function POST(req) {
 
   // Get the headers
   const headerPayload = headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
+  const svixId = headerPayload.get('svix-id')
+  const svixTimestamp = headerPayload.get('svix-timestamp')
+  const svixSignature = headerPayload.get('svix-signature')
 
   // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
+  if (!svixId || !svixTimestamp || !svixSignature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
@@ -36,10 +42,11 @@ export async function POST(req) {
 
   // Verify the payload with the headers
   try {
+    // eslint-disable-next-line no-unused-vars
     evt = wh.verify(body, {
-      'svix-id': svix_id,
-      'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature
+      'svix-id': svixId,
+      'svix-timestamp': svixTimestamp,
+      'svix-signature': svixSignature
     })
   } catch (err) {
     console.error('Error verifying webhook:', err)
@@ -48,12 +55,22 @@ export async function POST(req) {
     })
   }
 
-  // Get the ID and type
-  const { id } = evt.data
-  const eventType = evt.type
+  const { username, id } = JSON.parse(body).data
 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
+  const email = JSON.parse(body).data.email_addresses[0].email_address
+  try {
+    await dbConnect()
+    console.log('db connected')
+    const newUser = new User({
+      username,
+      userId: id,
+      email
+    })
 
-  return new Response('', { status: 200 })
+    const createUser = await newUser.save()
+
+    return NextResponse.json(createUser, { status: 201 })
+  } catch (error) {
+    return NextResponse.error(error)
+  }
 }
